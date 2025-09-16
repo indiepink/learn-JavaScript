a5c06abb96f4fca126dc414391daf1bb99ee1a92
indiepink <nobnel2050@gmail.com>
2025-09-16 10:27:22 +0900

    데이터 타입과 변수

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 변수
 const username = "송주령똥꾸멍"; //상수(const): 변할 수 없는 값
 console.log("🚀 ~ username:", username)
 
@@ -8,10 +9,29 @@ usermbti = "ESFJ"; // 수정
 console.log("🚀 ~ 변경된 usermbti:", usermbti);
 
 // 상수는 변경 불가능
-// username = "인기스타 송상훈";
+// username = "인기스타송상훈";
 // console.log("🚀 ~ 변경된 username:", username);
 
 
+// 데이터 타입
+const num = 12;
+const str = "텍스트";
+const bool =  true;
+const empty = undefined; 
+let empty2;
+
+//데이터 타입 확인: typeof
+console.log("num: "    + typeof num);    // number
+console.log("str: "    + typeof str);    // string
+console.log("bool: "   + typeof bool);   // boolean
+console.log("empty: "  + typeof empty);  // undefined 
+console.log("empty2: " + typeof empty2); // undefined
+
+/** 
+ * null과 undefined의 차이점
+ * null: 개발자가 의도를 가지고 값을 비움
+ * undefined: 개발자 의도와 상관없이 값이 없는 상태
+*/
 
 
 
