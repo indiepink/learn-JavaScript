130144621d5bce155ced5641668f07ddcaf31861
indiepink <nobnel2050@gmail.com>
2025-09-16 11:45:11 +0900

    함수 & return 학습

--- index.js
+++ index.js
@@ -33,6 +33,60 @@ console.log("empty2: " + typeof empty2); // undefined
  * undefined: 개발자 의도와 상관없이 값이 없는 상태
 */
 
+console.log("====================================================");
 
+// 함수
+// 함수 정의 & 선언
+function greeting() {
+  //실행할 로직들
+  //console.log("반갑습니다! " + username + "님");  
+  console.log(`반갑습니다! ${username}님 mbti는 ${usermbti}이시군요!`); 
+}
+
+// 화살표 함수
+const greeting2 = () => {
+  //실행할 로직들
+  //console.log("반갑습니다! " + username + "님");  
+  console.log(`반갑습니다! ${username}님 mbti는 ${usermbti}이시군요!`); 
+}
+
+// 함수 호출 & 실행
+greeting();  // 일반 함수
+greeting2(); // 화살표 함수
+
+// 함수의 인자와 매개변수
+const sum = (num1, num2) => {
+  //실행할 로직
+  //const result =  num1 + num2;
+  // console.log("🚀 ~ sum ~ result:", result);
+
+  // return 사용
+  // const result =  num1 + num2; 
+  // return result;
+  return num1 + num2;
+  console.log("return 끝!"); // return 이후 실행 안 됨
+}
+
+sum(11,22); // argument
+
+// 미션: sayHello라는 함수 만들기
+// 사용자 이름을 받아서 "반간습니다. OOO님"라는 텍스트를 console탭에 출력하는 기능 만들기
+
+const sayHello = (name) => {
+  console.log(`반갑습니다! ${name}님`);
+}
+
+sayHello("인기스타송상훈");
+
+//return 
+const add = sum(11, 22);
+console.log("🚀 ~ add:", add);
+
+const double = (num) => {
+  return num * 2 ;
+}  
+
+const doubleResult = double(add);
+console.log("🚀 ~ doubleResult:", doubleResult); // add(33) * 2 = 66
 
 
