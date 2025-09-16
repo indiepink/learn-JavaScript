392447d3c9188f7213f6bcff6ccc518dbfceef83
indiepink <nobnel2050@gmail.com>
2025-09-16 09:55:34 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const username = "송주령똥꾸멍"; //상수(const): 변할 수 없는 값
+console.log("🚀 ~ username:", username)
+
+let usermbti = "ISTJ"; //변수(let): 변할 수 있는 값
+console.log("🚀 ~ usermbti:", usermbti);
+
+usermbti = "ESFJ"; // 수정
+console.log("🚀 ~ 변경된 usermbti:", usermbti);
+
+// 상수는 변경 불가능
+// username = "인기스타 송상훈";
+// console.log("🚀 ~ 변경된 username:", username);
+
+
+
+
+
+
